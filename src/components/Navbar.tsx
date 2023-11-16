0467c61b89c5f28b442587d545309e98a11789fb
Elijah <elijahmujuni@gmail.com>
2023-11-16 18:35:35 +0300

    Added nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
-import { Container, Navbar as NavBts} from 'react-bootstrap'
+import { Container, Nav, Navbar as NavBts} from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 export function Navbar() {
     return (
             <NavBts className='bg-white shadow-sm mb-3'>
-                <Container>Nav</Container>
+                <Container>
+                <Nav>
+                    <NavLink to={'/'}>
+                        Home
+                    </NavLink>
+                    <NavLink to={'/store'}>
+                        Store
+                    </NavLink>
+                    <NavLink to={'/about'}>
+                        About
+                    </NavLink>
+                </Nav>
+                </Container>
             </NavBts>    
     )
 }
--- src/views/Store.tsx
+++ src/views/Store.tsx
@@ -4,8 +4,6 @@ export function Store () {
     return (
             <Container>
                 <h4>Store</h4>
-
-                <div className="p-3 mb-2 bg-gradient-info text-white">.bg-gradient-info</div>
             </Container>     
     )
 }
