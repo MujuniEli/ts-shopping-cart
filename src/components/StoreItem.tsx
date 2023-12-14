4afaf0fe63780ba23f932916155fa88523aac4bb
MujuniEli <elijahmujuni@gmail.com>
2023-12-14 21:59:53 +0300

    setting up store items render

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,5 @@
+export function StoreItem (id, name, price, imgUrl) {
+    return (
+
+    )
+}
--- src/views/Store.tsx
+++ src/views/Store.tsx
@@ -1,9 +1,19 @@
-import { Container } from "react-bootstrap"
+import { Container, Col, Row } from "react-bootstrap"
+import storeItems from "../data/items.json"
+import { StoreItem } from "../components/StoreItem"
 
 export function Store () {
     return (
             <Container>
                 <h4>Store</h4>
+                <Row md={2} xs={1} lg={3} className="g-3">
+                    {storeItems.map(item => (
+                        <Col key={item.id}>
+                        <StoreItem {...item} />
+                        </Col>
+                    ))}
+                    
+                </Row>
             </Container>     
     )
 }
