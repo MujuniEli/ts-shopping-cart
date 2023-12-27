d27375d2064eb3b4a99763c247ab70b5a2e761a1
MujuniEli <elijahmujuni@gmail.com>
2023-12-27 09:56:27 +0300

    added cart items update functionality

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utilities/formatCurrency"
+import { useShoppingCart } from "../context/shoppingCartContext"
 
 type StoreItemProps = {
     id: number
@@ -9,7 +10,13 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-    const quantity = 0
+    const {
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+    } = useShoppingCart()
+    const quantity = getItemQuantity(id)
     return (
                 <Card className="border-0 h-100">
                     <Card.Img 
@@ -25,17 +32,17 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                         </Card.Title>
                         <div className="mt-auto">
                             {quantity === 0 ? (
-                            <Button className="w-100">+ Add to Cart</Button>
+                            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add to Cart</Button>
                             ) : <div className="d-flex align-items-center flex-column" style={{gap: ".5rem"}}>
                                     <div className="d-flex align-items-center justify-content-center"
                                     style={{ gap: ".5rem"}}>
-                                        <Button>-</Button>
+                                        <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                                         <div>
                                             <span className="fs-3">{quantity}</span> in cart
                                         </div>
-                                        <Button>+</Button>
+                                        <Button onClick={() => increaseCartQuantity(id)}>+</Button>
                                     </div>
-                                    <Button variant="danger" size="sm">Remove</Button> 
+                                    <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>Remove</Button> 
                                 </div>}
                         </div>
                     </Card.Body>
--- src/context/shoppingCartContext.tsx
+++ src/context/shoppingCartContext.tsx
@@ -28,11 +28,52 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     function getItemQuantity(id: number) {
         return cartItems.find(item => item.id === id)?.quantity || 0
     }
+    
+    function increaseCartQuantity(id: number) {
+        setCartItems(currentItems => {
+            if (currentItems.find(item => item.id === id) == null) {
+                return [...currentItems, {id, quantity: 1}]
+            } else {
+                return currentItems.map(item => {
+                    if(item.id === id) {
+                        return {...item, quantity: item.quantity + 1}
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    }
+
+    function decreaseCartQuantity(id: number) {
+        setCartItems(currentItems => {
+            if (currentItems.find(item => item.id === id)?.quantity === 1) {
+                return currentItems.filter(item => item.id !== id)
+            } else {
+                return currentItems.map(item => {
+                    if(item.id === id) {
+                        return {...item, quantity: item.quantity - 1}
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    }
+
+    function removeFromCart (id: number) {
+        setCartItems(currentItems => {
+            return currentItems.filter(item => item.id !== id)
+        })
+    }
 
     return (
-            <ShoppingCartContext.Provider value={{ getItemQuantity(id) {
-                
-            },}}>
+            <ShoppingCartContext.Provider value={{ 
+                getItemQuantity, 
+            increaseCartQuantity, 
+            decreaseCartQuantity, 
+            removeFromCart 
+            }}>
                 {children}
             </ShoppingCartContext.Provider>
     )
